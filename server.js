1e2ea0cad7571edab7e346deeadb1cc1deb64073
Robert Rinehart <rinebob111185@gmail.com>
2018-09-25 10:14:39 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+app.use(express.static( __dirname + '/public/dist/public' ));
+app.use(express.static(__dirname + '/client/static/img'));
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var path = require('path');
+
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/ang_mean_belt_2');
+
+// require('./server/models/author.js')
+
+// require('./server/config/routes.js')(app)
+
+
+app.all("*", (req,res,next) => {
+
+res.sendFile(path.resolve("./public/dist/public/index.html"))
+});
+
+app.listen(8000, function() {
+	console.log("listening on port 8000");
+})
---
+++ server/config/routes.js
@@ -0,0 +1,16 @@
+// require('../models/movie.js')
+
+// var movies = require('../controllers/movies.js')
+
+
+module.exports = function(app){
+	
+	//index
+	
+
+	
+	
+
+
+
+}
